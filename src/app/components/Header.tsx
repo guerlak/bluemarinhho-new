161f44325fa7a7b764c566ccd900e2518fb017cf
Rafael Leite <guerlak@MacBook-Air-som-tillhor-Rafael.local>
2025-05-22 00:38:10 -0300

    new logo

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,20 @@
 // app/components/Header.tsx
+import Image from 'next/image';
 import Link from 'next/link';
+import logo from '../../../public/bluemarinho-logo.png'
+import { GiGuitar } from 'react-icons/gi';
+import { BiInfoCircle } from 'react-icons/bi';
+
+
 
 export default function Header() {
     return (
         <header className="flex justify-between items-center p-6 bg-white shadow-md">
-            <Link href="/"><h1 className="text-2xl font-bold">BLUE MARINHO</h1></Link>
+            <div className='w-30 lg:w-50'>
+                <Link href="/"><Image src={logo} alt='Blue Marinho Logo'></Image></Link>
+            </div>
             <nav className="space-x-4">
-
-                <Link href="/sobre">Sobre</Link>
+                <Link href="/sobre" className='text-xl hover:text-orange-400 transition duration-200'>Quem Somos</Link>
             </nav>
         </header >
     );
